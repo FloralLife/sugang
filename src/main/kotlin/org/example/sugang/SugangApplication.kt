f4ae46b034fabeeee41b3b474b0a450c7b6d39e2
yunoh-lee <yunoh.lee@linecorp.com>
2024-12-26 01:47:47 +0900

    📁 init : 프로젝트 초기화

---
+++ src/main/kotlin/org/example/sugang/SugangApplication.kt
@@ -0,0 +1,11 @@
+package org.example.sugang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SugangApplication
+
+fun main(args: Array<String>) {
+  runApplication<SugangApplication>(*args)
+}
